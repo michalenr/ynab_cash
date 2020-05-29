fe4de9a6e39e9101d9e3d7a578a3e684cd0faf28
michael <michale.ns32@gmail.com>
2020-05-29 16:52:22 +0300

    add basic style

--- components/EnterValueScreen.js
+++ components/EnterValueScreen.js
@@ -15,15 +15,19 @@ const EnterValueScreen = ({ next }) => {
   return (
     <>
       <View style={styles.view}>
-        <TextInput 
-          style={styles.textInput}
-          placeholder='How much did you spend?'
-          onChangeText={v => setValue(v)}
-          defaultValue={value}
-          keyboardType='numeric'
-        />
+        <View style={styles.textInputWrapper}>
+          <TextInput 
+            style={styles.textInput}
+            placeholder='How much did you spend?'
+            onChangeText={v => setValue(v)}
+            defaultValue={value}
+            keyboardType='numeric'
+          />
+        </View>
 
-      <Button title='Next' onPress={() => next(2)}/>
+        <View style={styles.buttonWrapper}>
+          <Button title='Next' onPress={() => next(2)}/>
+        </View>
 
       </View>
     </>
@@ -33,11 +37,26 @@ const EnterValueScreen = ({ next }) => {
 const styles = StyleSheet.create({
   view: {
     backgroundColor: 'white',
-    flex: 1
+    flex: 1,
+    paddingTop: 50
+  },
+  textInputWrapper: {
+    flex: 2
   },
   textInput: {
-    height: 200,
-  }
+    height: 100,
+    fontSize: 30,
+    marginLeft: 10,
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 50
+  },
+  buttonWrapper: {
+    flex: 1,
+    marginLeft: 30,
+    marginRight: 30
+  },
 });
 
 export default EnterValueScreen;
